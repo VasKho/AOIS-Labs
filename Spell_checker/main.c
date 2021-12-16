73d283f58bddacc25ede3771faa9063f97636e8c
VasKho <vasya.khoroshavin@gmail.com>
2021-12-16 23:40:50 +0300

    Now text dictionaries are available

--- Spell_checker/main.c
+++ Spell_checker/main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "spell_check.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    FILE* dictionary;
+    if (argc > 1) dictionary = fopen(argv[1], "rb");
+    else
+    {
+        printf("You should add dictionary file\n");
+        printf("Usage example: ./main dictionary.txt\n");
+        exit(1);
+    }
+    if (!dictionary)
+    {
+        printf("No such file or directory!\n");
+        exit(1);
+    }
+    fseek(dictionary, 0L, SEEK_END);
+    unsigned long size = ftell(dictionary);
+    fseek(dictionary, 0L, SEEK_SET);
     trie_node* root = gen_node();
-    insert(root, "shot");
-    insert(root, "shit");
-    insert(root, "shift");
-    insert(root, "tree");
+    char* word = (char*)malloc(sizeof(char)*size);
+    while (!feof(dictionary))
+    {
+        fscanf(dictionary, "%s\n", word);
+        insert(root, word);
+    }
 
     char query[30];
     while(1)
